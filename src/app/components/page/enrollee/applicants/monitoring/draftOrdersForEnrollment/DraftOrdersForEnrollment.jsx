bd6db150bb375bbb4b869a3d04c7cd7b340f2ca1
UFAVSUWT <oit@uf-vsuwt.ru>
2023-08-16 16:17:43 +0500

    docs

--- src/app/components/page/enrollee/applicants/monitoring/draftOrdersForEnrollment/DraftOrdersForEnrollment.jsx
+++ src/app/components/page/enrollee/applicants/monitoring/draftOrdersForEnrollment/DraftOrdersForEnrollment.jsx
@@ -1,10 +1,40 @@
 import React from "react";
-const DraftOrdersForEnrollment = () => {
+import { ReactComponent as DOC } from "../../../../../../assets/svg/office/word.svg";
+import { toggleClassName } from "../../../../../../utils/disabled";
+import spiski from "../../../../../../assets/doc/enrollee/Список рекомендованных к зачислению без среднего балла.docx"
+import { observer } from "mobx-react-lite";
+const DraftOrdersForEnrollment = observer(() => {
   return (
     <section>
       <h1>Проекты приказов на зачисление</h1>
-      <p>Проекты приказов на зачисление, будут объявлены позже</p>
+      <p>Список лиц рекомендованных к зачислению без среднего балла{" "}
+        <a
+          href={spiski}
+          target={"_blank"}
+          rel="noreferrer"
+          className={toggleClassName(
+            "education__link",
+            "education__link-white",
+            "education__link-black",
+            "education__link-contrast"
+          )}
+        >
+          <DOC width={"25px"} height={"25px"} />
+        </a>{" "}
+        <a
+          href={spiski}
+          target={"_blank"}
+          rel="noreferrer"
+          className={toggleClassName(
+            "education__link",
+            "education__link-white",
+            "education__link-black",
+            "education__link-contrast"
+          )}
+        >
+          скачать
+        </a></p>
     </section>
   );
-};
+});
 export default DraftOrdersForEnrollment;
