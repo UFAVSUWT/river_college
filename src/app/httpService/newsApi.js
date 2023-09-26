00228de3015bb26d0274928285fff330067b1d44
UFAVSUWT <oit@uf-vsuwt.ru>
2023-09-26 13:25:00 +0500

    feat: News http сервис

--- src/app/httpService/newsApi.js
+++ src/app/httpService/newsApi.js
@@ -1,14 +1,15 @@
 import { $authHost, $host } from "./index";
-import jwt_decode from "jwt-decode";
 
-export const loginAuth = async (login, password) => {
-  const { data } = await $host.post("api/user/login", { login, password });
-  localStorage.setItem("token", data.token);
-  return jwt_decode(data.token);
+export const createNews = async (news) => {
+  const { data } = await $authHost.post("api/news", { news });
+  return data;
 };
 
-export const check = async () => {
-  const { data } = await $authHost.get("api/user/auth");
-  localStorage.setItem("token", data.token);
-  return jwt_decode(data.token);
+export const fetchNews = async () => {
+  const { data } = await $host.get("api/news");
+  return data;
+};
+export const fetchOneNews = async (id) => {
+  const { data } = await $host.get("api/news" + id);
+  return data;
 };
