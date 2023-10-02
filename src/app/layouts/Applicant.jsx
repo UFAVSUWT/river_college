30df21dabd83e8aa903a1b208d63d1ad04949c12
Alexandr Koshelev <oit@uf-vsuwt.ru>
2023-10-02 16:22:29 +0500

    style: Изменил внешний вид лк пк

--- src/app/layouts/Applicant.jsx
+++ src/app/layouts/Applicant.jsx
@@ -5,7 +5,14 @@ import { toggleClassName } from "../utils/disabled";
 import { observer } from "mobx-react-lite";
 const Applicant = observer(() => {
   return (
-    <div className={` ${toggleClassName("_container-main min-height", "_container-white min-height-white"," _container-black min-height-black","_container-contrast min-height-contrast",)}`}>
+    <div
+      className={` ${toggleClassName(
+        "_container-main min-height",
+        "_container-white min-height-white",
+        " _container-black min-height-black",
+        "_container-contrast min-height-contrast"
+      )}`}
+    >
       <BreadCrumbs />
       <Outlet />
     </div>
--- src/app/layouts/auth/auth.jsx
+++ src/app/layouts/auth/auth.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { toggleClassName } from "../../utils/disabled";
 import { observer } from "mobx-react-lite";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/common/button/Button";
 import { Context } from "../../../index";
 import { logOut } from "../../utils/utils";
@@ -9,25 +9,53 @@ import { logOut } from "../../utils/utils";
 const Auth = observer(() => {
   const { user } = useContext(Context);
   const navigate = useNavigate();
-
+  const getRole = () => {
+    if (user.user.role === "ADMIN") {
+      return "Администратор";
+    }
+  };
   return (
     <section
       className={toggleClassName(
-        "education__container",
-        "education__container-white",
-        "education__container-black",
-        "education__container-contrast"
+        "_container education__container",
+        "_container education__container-white",
+        "_container education__container-black",
+        "_container education__container-contrast"
       )}
     >
       <h1 className="education__container-title">Личный кабинет</h1>
-      <div className="education__container-content flex min-height">
-        {" "}
-        <div>
-          {user.user.role === "ADMIN" ? (
-            <NavLink to="../admin">Панель администратора</NavLink>
-          ) : null}
+      <div
+        className={toggleClassName(
+          "education__container-content flex min-height",
+          "education__container-content flex min-height-white",
+          "education__container-content flex min-height-black",
+          "education__container-content flex min-height-contrast"
+        )}
+      >
+        <div
+          className={toggleClassName(
+            "auth-wrapper",
+            "auth-wrapper auth-wrapper-white",
+            "auth-wrapper auth-wrapper-black",
+            "auth-wrapper auth-wrapper-contrast"
+          )}
+        >
+          <div className="cards">
+            <h2>Информация о пользователе:</h2>
+            <span>Права доступа: {getRole()}</span>
+          </div>
+          <div className="cards">
+            <h2>Доступные возможности:</h2>
+            <div>
+              {user.user.role === "ADMIN" ? (
+                <Button onClick={() => navigate("../admin")}>
+                  Панель администратора
+                </Button>
+              ) : null}
+              <Button onClick={() => logOut(user, navigate)}>Выйти</Button>
+            </div>
+          </div>
         </div>
-        <Button onClick={() => logOut(user, navigate)}>Выйти</Button>
       </div>
     </section>
   );
