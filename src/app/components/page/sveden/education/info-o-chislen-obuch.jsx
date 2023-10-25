b60eee78115cca95537102e59e7665f6728ec2ea
Alexandr Koshelev <oit@uf-vsuwt.ru>
2023-10-25 11:41:24 +0500

    feat: Информация о численности обучающихся

---
+++ src/app/components/page/sveden/education/info-o-chislen-obuch.jsx
@@ -0,0 +1,288 @@
+import React from "react";
+import TableWrapper from "../../../common/tableWrapper/TableWrapper";
+const InfoOChislenObuch = () => {
+  return (
+    <TableWrapper
+      title="Информация о численности обучающихся по реализуемым образовательным
+        программам за счет бюджетных ассигнований федерального бюджета, бюджетов
+        субъектов Российской Федерации, местных бюджетов и по договорам об
+        образовании за счет средств физических и (или) юридических лиц на
+        01.10.2023:"
+    >
+      <table>
+        <tbody>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduCode" rowSpan="3">
+              Код
+            </td>
+            <td itemProp="eduName" rowSpan="3">
+              Наименование специальности, направления подготовки, шифр и
+              наименование научной специальности
+            </td>
+            <td itemProp="eduLevel" rowSpan="3">
+              Уровень образования
+            </td>
+            <td itemProp="eduForm" rowSpan="3">
+              Формы обучения
+            </td>
+            <td colSpan="8">
+              Численность обучающихся за счет (количество человек):
+            </td>
+            <td itemProp="numberAll" rowSpan="3">
+              Общая численность обучающихся
+            </td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td colSpan="2">бюджетных ассигнований федерального бюджета</td>
+            <td colSpan="2">бюджетов субъектов Российской Федерации</td>
+            <td colSpan="2">местных бюджетов </td>
+            <td colSpan="2">средств физических и (или) юридических лиц</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="numberBF">всего</td>
+            <td itemProp="numberBFF">в том числе иностранных граждан</td>
+            <td itemProp="numberBR">всего</td>
+            <td itemProp="numberBRF">в том числе иностранных граждан</td>
+            <td itemProp="numberBM">всего</td>
+            <td itemProp="numberBMF">в том числе иностранных граждан</td>
+            <td itemProp="numberP">всего</td>
+            <td itemProp="numberPF">в том числе иностранных граждан</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduCode">-</td>
+            <td itemProp="eduName">-</td>
+            <td itemProp="eduLevel">высшее образование - бакалавриат</td>
+            <td itemProp="eduForm">-</td>
+            <td itemProp="numberBF">-</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">-</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">-</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduCode">-</td>
+            <td itemProp="eduName">-</td>
+            <td itemProp="eduLevel">высшее образование - специалитет</td>
+            <td itemProp="eduForm">-</td>
+            <td itemProp="numberBF">-</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">-</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">-</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduCode">-</td>
+            <td itemProp="eduName">-</td>
+            <td itemProp="eduLevel">высшее образование - магистратура</td>
+            <td itemProp="eduForm">-</td>
+            <td itemProp="numberBF">-</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">-</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">-</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduCode">-</td>
+            <td itemProp="eduName">-</td>
+            <td itemProp="eduLevel">
+              высшее образование - подготовка кадров высшей квалификации
+            </td>
+            <td itemProp="eduForm">-</td>
+            <td itemProp="numberBF">-</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">-</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">-</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduCode" rowSpan="3">
+              23.02.01
+            </td>
+            <td itemProp="eduName" rowSpan="3">
+              Организация перевозок и управление на транспорте
+            </td>
+            <td itemProp="eduLevel" rowSpan="3">
+              среднее профессиональное образование
+            </td>
+            <td itemProp="eduForm">очная</td>
+            <td itemProp="numberBF">92</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">5</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">97</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduForm">очно-заочная</td>
+            <td itemProp="numberBF">-</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">-</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">-</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduForm">заочная</td>
+            <td itemProp="numberBF">3</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">1</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">4</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td rowSpan="3">23.02.03</td>
+            <td rowSpan="3">Судовождение</td>
+            <td rowSpan="3">среднее профессиональное образование</td>
+            <td itemProp="eduForm">очная</td>
+            <td itemProp="numberBF">293</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">8</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">301</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduForm">очно-заочная</td>
+            <td itemProp="numberBF">-</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">-</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">-</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduForm">заочная</td>
+            <td itemProp="numberBF">5</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">47</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">52</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduCode" rowSpan="3">
+              23.02.05
+            </td>
+            <td itemProp="eduName" rowSpan="3">
+              Эксплуатация судовых энергетических установок
+            </td>
+            <td itemProp="eduLevel" rowSpan="3">
+              среднее профессиональное образование
+            </td>
+            <td itemProp="eduForm">очная</td>
+            <td itemProp="numberBF">179</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">4</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">183</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduForm">очно-заочная</td>
+            <td itemProp="numberBF">-</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">-</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">-</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduForm">заочная</td>
+            <td itemProp="numberBF">5</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">-</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">5</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td rowSpan="3">23.02.06</td>
+            <td rowSpan="3">
+              Эксплуатация судового электрооборудования и средств автоматики
+            </td>
+            <td rowSpan="3">среднее профессиональное образование</td>
+            <td itemProp="eduForm">очная</td>
+            <td itemProp="numberBF">197</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">1</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">198</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduForm">очно-заочная</td>
+            <td itemProp="numberBF">-</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">-</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">-</td>
+          </tr>
+          <tr itemProp="eduChislen">
+            <td itemProp="eduForm">заочная</td>
+            <td itemProp="numberBF">-</td>
+            <td itemProp="numberBFF">-</td>
+            <td itemProp="numberBR">-</td>
+            <td itemProp="numberBRF">-</td>
+            <td itemProp="numberBM">-</td>
+            <td itemProp="numberBMF">-</td>
+            <td itemProp="numberP">-</td>
+            <td itemProp="numberPF">-</td>
+            <td itemProp="numberAll">-</td>
+          </tr>
+        </tbody>
+      </table>
+    </TableWrapper>
+  );
+};
+export default InfoOChislenObuch;
